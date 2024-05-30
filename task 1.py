18a7553a1ea55a5a5e84df1c4a157b086c0137ac
vishal1500 <vishal1412009@gmail.com>
2024-05-30 21:13:38 +0530

    Delete task 1.py

--- task 1.py
+++
@@ -1,9 +0,0 @@
-subjects = ["I", "You"]
-verbs = ["Play", "Love"]
-objects = ["Cricket", "Ludo"]
-
-for subject in subjects:
-    for verb in verbs:
-        for object in objects:
-            sentence = f"{subject} {verb} {object}."
-            print(sentence)
